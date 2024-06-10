39103c9682853c5d256cae801ed7e804ac097a84
MatheusEngSoft <matheus.engsof@gmail.com>
2024-06-10 14:28:35 -0300

    criado as rotas de noticias

--- back/index.js
+++ back/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors')
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const usuarioModel = require('./src/module/usuario/usuario.model');
+const noticiaModel = require('./src/module/noticia/noticia.model');
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors())
+
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ message: 'Algo deu errado!' });
+});
+
+// Rota de login
 app.post('/login', async (req, res) => {
   const { email, senha } = req.body;
 
@@ -37,13 +48,14 @@ app.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Senha incorreta' });
     }
 
-    const token =  jwt.sign({ id: usuarioExistente._id}, 'dnc')
-    return res.status(200).json({ message: 'Login bem-sucedido', usuario: usuarioExistente });
+    const token = jwt.sign({ id: usuarioExistente._id }, 'dnc');
+    return res.status(200).json({ message: 'Login bem-sucedido', usuario: usuarioExistente, token });
   } catch (error) {
     return res.status(500).json({ message: 'Erro ao se conectar' });
   }
 });
 
+// Rota de criação de usuário
 app.post('/usuarios', async (req, res) => {
   const { email, senha, nome } = req.body;
 
@@ -70,7 +82,7 @@ app.post('/usuarios', async (req, res) => {
       return res.status(400).json({ message: 'Usuário já existe' });
     }
 
-    const senhaCriptografada = bcrypt.hashSync(senha, 10);
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
     const usuario = await usuarioModel.create({
       nome,
       email,
@@ -83,23 +95,63 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
-app.get('/usuarios', async (req, res) => {
+// Rota para buscar usuários
+app.get('/usuarios', async (_, res) => {
   try {
     const usuarios = await usuarioModel.find();
     return res.status(200).json(usuarios);
   } catch (error) {
+    return res.status
     return res.status(500).json({ message: 'Erro ao buscar usuários' });
   }
 });
 
-app.get('/noticias', (req, res) => {
-  return res.status(200).json([]);
+// Rota para buscar notícias
+app.get('/noticias', async (req, res) => {
+    let filtroCategoria = {}
+    if(req.query.categoria){
+      filtroCategoria = { categoria:req.query.categoria }
+    }
+  try {
+    const noticias = await noticiaModel.find(filtroCategoria);
+    return res.status(200).json(noticias);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao buscar notícias' });
+  }
 });
 
-app.post('/noticias', (req, res) => {
-  return res.status(201).json([]);
+// Rota para criar notícias
+app.post('/noticias', async (req, res) => {
+  const { titulo, img, texto, categoria } = req.body;
+
+  if (!titulo) {
+    return res.status(400).json({ message: 'Título é obrigatório' });
+  }
+  if (!img) {
+    return res.status(400).json({ message: 'Imagem é obrigatória' });
+  }
+  if (!texto) {
+    return res.status(400).json({ message: 'Texto é obrigatório' });
+  }
+  if (!categoria) {
+    return res.status(400).json({ message: 'Categoria é obrigatória' });
+  }
+
+  try {
+    const noticia = await noticiaModel.create({
+      titulo,
+      img,
+      texto,
+      categoria,
+    });
+    return res.status(201).json(noticia);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao criar notícia' });
+  }
 });
 
-app.listen(8080, () => {
-  console.log('Servidor está aberto na porta 8080');
+// Inicializa o servidor
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor está aberto na porta ${PORT}`);
 });
--- back/src/module/noticia/noticia.model.js
+++ back/src/module/noticia/noticia.model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('../../config/mongo')
 const { Schema } = mongoose;
 
 const noticiaSchema = new Schema(
@@ -6,6 +6,7 @@ const noticiaSchema = new Schema(
     titulo: String, 
     img: String,
     texto: String,
+    categoria: String
   }, 
   { 
     timestamps:true 
@@ -13,4 +14,4 @@ const noticiaSchema = new Schema(
 );
 
 const NoticiaModel = mongoose.model('Noticia', noticiaSchema)
-export default NoticiaModel
+module.exports = NoticiaModel
